e3afe9424034b907aee58f8b70aa38c613227938
Uday Singhal <udayalwayshere@gmail.com>
2025-07-13 02:19:33 +0530

    Fix hamburger on mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,13 @@ console.log("Grocery Grid loaded ✨");
 
 // Future feature: toggle nav on mobile or extra animations here
 
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.querySelector('.navbar nav ul');
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById('hamburger');
+  const navMenu = document.getElementById('nav-menu');
 
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
-  hamburger.classList.toggle('active');
+  hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+  });
 });
+
