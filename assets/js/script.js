571f6c71638253915dbb6d1ae13a3c348d59d345
Uday Singhal <udayalwayshere@gmail.com>
2025-07-13 02:33:56 +0530

    Fix hamburger4 on mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
 console.log("Grocery Grid loaded ✨");
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.getElementById("hamburger");
   const navMenu = document.getElementById("nav-menu");
 
-  hamburger.addEventListener("click", () => {
-    console.log("clicked");
-    navMenu.classList.toggle("active");
-  });
+  if (hamburger && navMenu) {
+    hamburger.addEventListener("click", function () {
+      console.log("clicked ✅");
+      navMenu.classList.toggle("active");
+    });
+  } else {
+    console.warn("❌ navMenu or hamburger not found in DOM");
+  }
 });
