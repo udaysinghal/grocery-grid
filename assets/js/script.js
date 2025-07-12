5a619d2df8ccddc533b4b42533a7b19d2a0c7625
Uday Singhal <udayalwayshere@gmail.com>
2025-07-13 02:40:13 +0530

    Fix hamburger final on mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.getElementById("hamburger");
   const navMenu = document.getElementById("nav-menu");
 
-  if (hamburger && navMenu) {
-    hamburger.addEventListener("click", function () {
-      console.log("clicked ✅");
-      navMenu.classList.toggle("active");
-    });
-  } else {
-    console.warn("❌ navMenu or hamburger not found in DOM");
-  }
+  hamburger.addEventListener("click", function () {
+    console.log("clicked ✅");
+    navMenu.classList.toggle("active");
+  });
 });
